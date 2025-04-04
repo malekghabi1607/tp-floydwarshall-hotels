7f80d55338ad8e72ec44c2ab6b056b5d3e79f560
Malek GHABI <145768940+malekghabi1607@users.noreply.github.com>
2025-04-04 15:57:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,26 @@
 using namespace std;
 
 int main() {
-	cout << "Écrire dans le main vos propres tests" << endl;
+	const int INF = 9999;
+	int A[N][N] = {
+		{0,   3,   INF, 7},
+		{8,   0,   2,   INF},
+		{5,   INF, 0,   1},
+		{2,   INF, INF, 0}
+	};
+	int D[N][N];
+	int n = 4;
+
+	floydwarshall(A, D, n);
+
+	cout << "Matrice des plus courts chemins :" << endl;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (D[i][j] == INF)
+				cout << "INF\t";
+			else
+				cout << D[i][j] << "\t";
+		}
+		cout << endl;
+	}
 }
